ef3b6723fb521c1bd82db1ed0f4f5cc7a2ca7555
Sehrish khan <sehrishahsan1121@gmail.com>
2024-06-06 11:50:57 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000; // Dollar
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code ");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select options",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let ammountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= ammountAns.amount;
+        console.log("Your remaining balance is:" + myBalance);
+    }
+    else if (operationAns.operation === "check balance")
+        console.log("your balance is" + myBalance);
+}
+else {
+    console.log("Incorrect pin number");
+}
